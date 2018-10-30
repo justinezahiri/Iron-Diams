a031741d251c27bcbb96e5fc17634eb11ba4b463
justinezahiri <justinezahiri@gmail.com>
2018-10-30 13:32:27 +0100

    diamonds random

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
+var NUMBER_OF_LINES = 5;
+var NUMBER_OF_COLUMNS = 5;
+
 $(document).ready(function(){ 
+    generateGameBoard();
+});
 
-    // GENERAL ELEMENTS
-    var diamOne = $(".diam-one");//always visible
-    var diamTwo =  $(".diam-two");
-    var diamThree = $(".diam-three");
-    var diamFour = $(".diam-four");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 
-    var diamsArray = [
-        diamOne,
-        diamTwo,
-        diamThree,
-        diamFour,
-    ];
+function generateGameBoard() {
 
-function showRandomDiams() {
-    var randomDiam = Math.floor(Math.random() * 2);
-    //show or hide if 0 or 1
-    if (randomDiam = 0) {
-        false;
-    }
-    if (randomDiam = 1) {
-        true;
-    }
-    
-}
+    var boardHtml = "";
 
-})
+    for (var i = 0; i < NUMBER_OF_LINES; i++) {
+        boardHtml += "<div class='board-line'>";
+
+        for (var j = 0; j < NUMBER_OF_COLUMNS; j++) {
+            var diamondsCount = 1 + getRandomInt(4);
+            boardHtml += "<div class='square diamonds-count-" + diamondsCount + "'></div>";
+        }
+
+        boardHtml += "</div>";
+    };
+
+    $('.game-square').append(boardHtml);
+
+};
